588e5058cd5b7d42d0ab48e43720d067cea53a5d
Nikola Cetic <nikolacetic8@gmail.com>
2023-03-16 13:24:55 +0100

    lab 1-Done!

---
+++ de1-soc-demo/hw/quartus/unsaved/unsaved_bb.v
@@ -0,0 +1,10 @@
+
+module unsaved (
+	clk_clk,
+	reset_reset_n,
+	pll_0_sdram_clk);	
+
+	input		clk_clk;
+	input		reset_reset_n;
+	output		pll_0_sdram_clk;
+endmodule
---
+++ de1-soc-demo/hw/quartus/unsaved/unsaved_inst.vhd
@@ -0,0 +1,15 @@
+	component unsaved is
+		port (
+			clk_clk         : in  std_logic := 'X'; -- clk
+			reset_reset_n   : in  std_logic := 'X'; -- reset_n
+			pll_0_sdram_clk : out std_logic         -- clk
+		);
+	end component unsaved;
+
+	u0 : component unsaved
+		port map (
+			clk_clk         => CONNECTED_TO_clk_clk,         --         clk.clk
+			reset_reset_n   => CONNECTED_TO_reset_reset_n,   --       reset.reset_n
+			pll_0_sdram_clk => CONNECTED_TO_pll_0_sdram_clk  -- pll_0_sdram.clk
+		);
+
