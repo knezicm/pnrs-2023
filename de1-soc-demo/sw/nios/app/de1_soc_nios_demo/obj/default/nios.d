b819f518488e878760fdc5ff3542ba218c799003
Nemanja0 <ncenic39@gmail.com>
2023-03-26 20:38:01 +0200

    Uradjena treca vjezba

---
+++ de1-soc-demo/sw/nios/app/de1_soc_nios_demo/obj/default/nios.d
@@ -0,0 +1,16 @@
+obj/default/nios.o: nios.c \
+ C:/Users/Medion/Desktop/4.godina/8.semestar/PNRS/pnrs-2023/de1-soc-demo/sw/nios/app/de1_soc_nios_demo_bsp/HAL/inc/io.h \
+ C:/Users/Medion/Desktop/4.godina/8.semestar/PNRS/pnrs-2023/de1-soc-demo/sw/nios/app/de1_soc_nios_demo_bsp/HAL/inc/alt_types.h \
+ C:/Users/Medion/Desktop/4.godina/8.semestar/PNRS/pnrs-2023/de1-soc-demo/sw/nios/app/de1_soc_nios_demo_bsp/drivers/inc/altera_avalon_pio_regs.h \
+ C:/Users/Medion/Desktop/4.godina/8.semestar/PNRS/pnrs-2023/de1-soc-demo/sw/nios/app/de1_soc_nios_demo_bsp/system.h \
+ C:/Users/Medion/Desktop/4.godina/8.semestar/PNRS/pnrs-2023/de1-soc-demo/sw/nios/app/de1_soc_nios_demo_bsp/linker.h
+
+C:/Users/Medion/Desktop/4.godina/8.semestar/PNRS/pnrs-2023/de1-soc-demo/sw/nios/app/de1_soc_nios_demo_bsp/HAL/inc/io.h:
+
+C:/Users/Medion/Desktop/4.godina/8.semestar/PNRS/pnrs-2023/de1-soc-demo/sw/nios/app/de1_soc_nios_demo_bsp/HAL/inc/alt_types.h:
+
+C:/Users/Medion/Desktop/4.godina/8.semestar/PNRS/pnrs-2023/de1-soc-demo/sw/nios/app/de1_soc_nios_demo_bsp/drivers/inc/altera_avalon_pio_regs.h:
+
+C:/Users/Medion/Desktop/4.godina/8.semestar/PNRS/pnrs-2023/de1-soc-demo/sw/nios/app/de1_soc_nios_demo_bsp/system.h:
+
+C:/Users/Medion/Desktop/4.godina/8.semestar/PNRS/pnrs-2023/de1-soc-demo/sw/nios/app/de1_soc_nios_demo_bsp/linker.h:
