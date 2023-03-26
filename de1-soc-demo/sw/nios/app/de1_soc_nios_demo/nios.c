e3e87c368a9fea948051f557af26af5e11d2be94
alexBeslic <beslic.alex@gmail.com>
2023-03-26 12:25:01 +0200

    Lab 3 - uradjen

---
+++ de1-soc-demo/sw/nios/app/de1_soc_nios_demo/nios.c
@@ -0,0 +1,60 @@
+/*
+ * nios.c
+ *
+ *  Created on: 2023-03-24
+ *      Author: Aleksandar Beslic
+ */
+
+
+
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include "io.h"
+#include "altera_avalon_pio_regs.h"
+#include "system.h"
+
+#define SLEEP_DELAY_US (100 * 1000)
+
+void rotate_leds()
+{
+	int leds_mask = 0x01;
+
+	/* 0/1 = left/right direction */
+	int led_direction = 0;
+
+	while (true)
+	{
+		uint32_t switches_value = IORD_ALTERA_AVALON_PIO_DATA(SWITCHES_0_BASE);
+		uint32_t leds_value = ~leds_mask;
+
+		/* Only turns on leds which have their corresponding switch enabled */
+		IOWR_ALTERA_AVALON_PIO_DATA(LEDS_0_BASE, leds_value & switches_value);
+
+		usleep(SLEEP_DELAY_US);
+
+		if (led_direction == 0)
+		{
+			leds_mask <<= 1;
+			if (leds_mask == (0x01 << (LEDS_0_DATA_WIDTH - 1)))
+			{
+				led_direction = 1;
+			}
+		}
+		else
+		{
+			leds_mask >>= 1;
+			if (leds_mask == 0x01)
+			{
+				led_direction = 0;
+			}
+		}
+	}
+}
+
+int main()
+{
+	rotate_leds();
+	return 0;
+}
---
+++ de1-soc-demo/sw/nios/app/de1_soc_nios_demo/obj/default/nios.d
@@ -0,0 +1,11 @@
+obj/default/nios.o: nios.c \
+ d\:\fax\projektovanje_namjenskih_racunarski_struktura\pnrs-2023\de1-soc-demo\sw\nios\app\de1_soc_nios_demo_bsp\hal\inc\io.h \
+ d\:\fax\projektovanje_namjenskih_racunarski_struktura\pnrs-2023\de1-soc-demo\sw\nios\app\de1_soc_nios_demo_bsp\hal\inc\alt_types.h \
+ d\:\fax\projektovanje_namjenskih_racunarski_struktura\pnrs-2023\de1-soc-demo\sw\nios\app\de1_soc_nios_demo_bsp\drivers\inc\altera_avalon_pio_regs.h \
+ d\:/Fax/projektovanje_namjenskih_racunarski_struktura/pnrs-2023/de1-soc-demo/sw/nios/app/de1_soc_nios_demo_bsp/system.h \
+ d\:/Fax/projektovanje_namjenskih_racunarski_struktura/pnrs-2023/de1-soc-demo/sw/nios/app/de1_soc_nios_demo_bsp/linker.h
+d\:\fax\projektovanje_namjenskih_racunarski_struktura\pnrs-2023\de1-soc-demo\sw\nios\app\de1_soc_nios_demo_bsp\hal\inc\io.h:
+d\:\fax\projektovanje_namjenskih_racunarski_struktura\pnrs-2023\de1-soc-demo\sw\nios\app\de1_soc_nios_demo_bsp\hal\inc\alt_types.h:
+d\:\fax\projektovanje_namjenskih_racunarski_struktura\pnrs-2023\de1-soc-demo\sw\nios\app\de1_soc_nios_demo_bsp\drivers\inc\altera_avalon_pio_regs.h:
+d\:/Fax/projektovanje_namjenskih_racunarski_struktura/pnrs-2023/de1-soc-demo/sw/nios/app/de1_soc_nios_demo_bsp/system.h:
+d\:/Fax/projektovanje_namjenskih_racunarski_struktura/pnrs-2023/de1-soc-demo/sw/nios/app/de1_soc_nios_demo_bsp/linker.h:
