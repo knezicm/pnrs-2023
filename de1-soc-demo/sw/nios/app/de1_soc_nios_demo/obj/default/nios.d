157925d772e30c04d1ff2bbad2216c8a81a57f66
Nikola Cetic <nikolacetic8@gmail.com>
2023-03-28 14:33:12 +0200

    Lab 3-Done!

---
+++ de1-soc-demo/sw/nios/app/de1_soc_nios_demo/obj/default/nios.d
@@ -0,0 +1,11 @@
+obj/default/nios.o: nios.c \
+ c\:\pnrs-2023\de1-soc-demo\sw\nios\app\de1_soc_nios_demo_bsp\hal\inc\io.h \
+ c\:\pnrs-2023\de1-soc-demo\sw\nios\app\de1_soc_nios_demo_bsp\hal\inc\alt_types.h \
+ c\:\pnrs-2023\de1-soc-demo\sw\nios\app\de1_soc_nios_demo_bsp\drivers\inc\altera_avalon_pio_regs.h \
+ c\:/pnrs-2023/de1-soc-demo/sw/nios/app/de1_soc_nios_demo_bsp/system.h \
+ c\:/pnrs-2023/de1-soc-demo/sw/nios/app/de1_soc_nios_demo_bsp/linker.h
+c\:\pnrs-2023\de1-soc-demo\sw\nios\app\de1_soc_nios_demo_bsp\hal\inc\io.h:
+c\:\pnrs-2023\de1-soc-demo\sw\nios\app\de1_soc_nios_demo_bsp\hal\inc\alt_types.h:
+c\:\pnrs-2023\de1-soc-demo\sw\nios\app\de1_soc_nios_demo_bsp\drivers\inc\altera_avalon_pio_regs.h:
+c\:/pnrs-2023/de1-soc-demo/sw/nios/app/de1_soc_nios_demo_bsp/system.h:
+c\:/pnrs-2023/de1-soc-demo/sw/nios/app/de1_soc_nios_demo_bsp/linker.h:
