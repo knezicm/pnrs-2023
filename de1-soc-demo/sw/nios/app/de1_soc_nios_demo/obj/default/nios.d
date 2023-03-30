ce814f6844bca325123f5e18778ed74f23be4a71
BojanB1997 <58489629+BojanB1997@users.noreply.github.com>
2023-03-30 14:00:24 +0200

    Uradjena treca laboratorijska vjezba.

---
+++ de1-soc-demo/sw/nios/app/de1_soc_nios_demo/obj/default/nios.d
@@ -0,0 +1,16 @@
+obj/default/nios.o: nios.c \
+ c:\users\public\de1-soc-demo\sw\nios\app\de1_soc_nios_demo_bsp\hal\inc\io.h \
+ c:\users\public\de1-soc-demo\sw\nios\app\de1_soc_nios_demo_bsp\hal\inc\alt_types.h \
+ c:\users\public\de1-soc-demo\sw\nios\app\de1_soc_nios_demo_bsp\drivers\inc\altera_avalon_pio_regs.h \
+ C:/Users/Public/de1-soc-demo/sw/nios/app/de1_soc_nios_demo_bsp/system.h \
+ C:/Users/Public/de1-soc-demo/sw/nios/app/de1_soc_nios_demo_bsp/linker.h
+
+c:\users\public\de1-soc-demo\sw\nios\app\de1_soc_nios_demo_bsp\hal\inc\io.h:
+
+c:\users\public\de1-soc-demo\sw\nios\app\de1_soc_nios_demo_bsp\hal\inc\alt_types.h:
+
+c:\users\public\de1-soc-demo\sw\nios\app\de1_soc_nios_demo_bsp\drivers\inc\altera_avalon_pio_regs.h:
+
+C:/Users/Public/de1-soc-demo/sw/nios/app/de1_soc_nios_demo_bsp/system.h:
+
+C:/Users/Public/de1-soc-demo/sw/nios/app/de1_soc_nios_demo_bsp/linker.h:
