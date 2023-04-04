d4c9f445f170c2b8771ac860bc7aa5dea7f29a32
popovict <tanja.popovic@student.etf.unibl.org>
2023-04-04 20:21:27 +0200

    Uradjena treca laboratorijska vjezba

---
+++ de1-soc-demo/sw/nios/app/de1_soc_nios_demo/obj/default/nios.d
@@ -0,0 +1,16 @@
+obj/default/nios.o: nios.c \
+ d:\de1-soc-demo\sw\nios\app\de1_soc_nios_demo_bsp\hal\inc\io.h \
+ d:\de1-soc-demo\sw\nios\app\de1_soc_nios_demo_bsp\hal\inc\alt_types.h \
+ d:\de1-soc-demo\sw\nios\app\de1_soc_nios_demo_bsp\drivers\inc\altera_avalon_pio_regs.h \
+ D:/de1-soc-demo/sw/nios/app/de1_soc_nios_demo_bsp/system.h \
+ D:/de1-soc-demo/sw/nios/app/de1_soc_nios_demo_bsp/linker.h
+
+d:\de1-soc-demo\sw\nios\app\de1_soc_nios_demo_bsp\hal\inc\io.h:
+
+d:\de1-soc-demo\sw\nios\app\de1_soc_nios_demo_bsp\hal\inc\alt_types.h:
+
+d:\de1-soc-demo\sw\nios\app\de1_soc_nios_demo_bsp\drivers\inc\altera_avalon_pio_regs.h:
+
+D:/de1-soc-demo/sw/nios/app/de1_soc_nios_demo_bsp/system.h:
+
+D:/de1-soc-demo/sw/nios/app/de1_soc_nios_demo_bsp/linker.h:
