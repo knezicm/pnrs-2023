b49dc642e27f7fc74f5d9517602406795470f3a6
popovict <tanja.popovic@student.etf.unibl.org>
2023-05-04 21:46:47 +0200

    Uradjena peta laboratorijska vjezba

---
+++ de1-soc-demo/sw/hps/app/de1_soc_demo_hps_linux/Debug/hps_linux.d
@@ -0,0 +1,25 @@
+hps_linux.d: ../hps_linux.c \
+ C:\intelFPGA\17.0\embedded\ip\altera\hps\altera_hps\hwlib\include/alt_generalpurpose_io.h \
+ C:\intelFPGA\17.0\embedded\ip\altera\hps\altera_hps\hwlib\include/hwlib.h \
+ ../hps_linux.h \
+ C:\intelFPGA\17.0\embedded\ip\altera\hps\altera_hps\hwlib\include\soc_cv_av/socal/hps.h \
+ ../../hps_soc_system.h \
+ C:\intelFPGA\17.0\embedded\ip\altera\hps\altera_hps\hwlib\include/hwlib.h \
+ C:\intelFPGA\17.0\embedded\ip\altera\hps\altera_hps\hwlib\include\soc_cv_av/socal/alt_gpio.h \
+ C:\intelFPGA\17.0\embedded\ip\altera\hps\altera_hps\hwlib\include\soc_cv_av/socal/socal.h
+
+C:\intelFPGA\17.0\embedded\ip\altera\hps\altera_hps\hwlib\include/alt_generalpurpose_io.h:
+
+C:\intelFPGA\17.0\embedded\ip\altera\hps\altera_hps\hwlib\include/hwlib.h:
+
+../hps_linux.h:
+
+C:\intelFPGA\17.0\embedded\ip\altera\hps\altera_hps\hwlib\include\soc_cv_av/socal/hps.h:
+
+../../hps_soc_system.h:
+
+C:\intelFPGA\17.0\embedded\ip\altera\hps\altera_hps\hwlib\include/hwlib.h:
+
+C:\intelFPGA\17.0\embedded\ip\altera\hps\altera_hps\hwlib\include\soc_cv_av/socal/alt_gpio.h:
+
+C:\intelFPGA\17.0\embedded\ip\altera\hps\altera_hps\hwlib\include\soc_cv_av/socal/socal.h:
