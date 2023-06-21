83ceb0cc7141f4f0e6d36f36504a649af4debd8d
BojanB1997 <58489629+BojanB1997@users.noreply.github.com>
2023-06-21 14:09:40 +0200

    Zavrsen Nios_0 (bez testiranja mailboxa).

--- de1-soc-audio/sw/nios/app_0/de1_soc_user_input/user_input.c
+++ de1-soc-audio/sw/nios/app_0/de1_soc_user_input/user_input.c
@@ -10,11 +10,136 @@
 #include "altera_avalon_timer_regs.h"
 #include "altera_avalon_pio_regs.h"
 #include "altera_avalon_mailbox_simple.h"
+#include <io.h>
 
 altera_avalon_mailbox_dev* mailbox_0;
+volatile int mode = 0;
 
-int main(void)
+void clear_display()
 {
+	/* Clear Hex 0 to 4 */
+	IOWR(HEX_0_BASE, 0, 0x000000FF);
+	IOWR(HEX_1_BASE, 0, 0x000000FF);
+	IOWR(HEX_2_BASE, 0, 0x000000FF);
+	IOWR(HEX_3_BASE, 0, 0x000000FF);
+	IOWR(HEX_4_BASE, 0, 0x000000FF);
+}
 
-	return 0;
+void display_start()
+{
+	/* Display Hex_0 letter S */
+	IOWR(HEX_0_BASE, 0, 0x00000012);
+
+	/* Display Hex_1 letter t */
+	IOWR(HEX_1_BASE, 0, 0x00000038);
+
+	/* Display Hex_2 letter A */
+	IOWR(HEX_2_BASE, 0, 0x00000001);
+
+	/* Display Hex_3 letter r */
+	IOWR(HEX_3_BASE, 0, 0x0000003D);
+
+	/* Display Hex_4 letter t */
+	IOWR(HEX_4_BASE, 0, 0x00000038);
+}
+
+void display_play()
+{
+	/* Display Hex_0 letter P */
+	IOWR(HEX_0_BASE, 0, 0x00000021);
+
+	/* Display Hex_1 letter L */
+	IOWR(HEX_1_BASE, 0, 0x00000078);
+
+	/* Display Hex_2 letter A */
+	IOWR(HEX_2_BASE, 0, 0x00000001);
+
+	/* Display Hex_3 letter Y */
+	IOWR(HEX_3_BASE, 0, 0x0000000A);
+}
+
+void display_rec()
+{
+	/* Display Hex_0 letter r */
+	IOWR(HEX_0_BASE, 0, 0x0000003D);
+
+	/* Display Hex_1 letter E */
+	IOWR(HEX_1_BASE, 0, 0x00000030);
+
+	/* Display Hex_2 letter C */
+	IOWR(HEX_2_BASE, 0, 0x00000070);
+}
+
+void display_loop()
+{
+	/* Display Hex_0 letter L */
+	IOWR(HEX_0_BASE, 0, 0x00000078);
+
+	/* Display Hex_1 letter O */
+	IOWR(HEX_1_BASE, 0, 0x00000040);
+
+	/* Display Hex_2 letter O */
+	IOWR(HEX_2_BASE, 0, 0x00000040);
+
+	/* Display Hex_3 letter P */
+	IOWR(HEX_3_BASE, 0, 0x00000021);
+}
+
+void display_mode(int mode)
+{
+	/* Mode: 1 - play, 2 - record, 3 - loopback. */
+	if(mode == 1)
+	{
+		display_play();
+	}
+	else if(mode == 2)
+	{
+		display_rec();
+	}
+	else if(mode == 3)
+	{
+		display_loop();
+	}
+}
+
+int main()
+{
+	unsigned short buttons;
+
+	/* Inicijalizacija mailboxa. */
+	mailbox_0 = altera_avalon_mailbox_open(MAILBOX_0_NAME, NULL, NULL);
+
+	clear_display();
+	display_start();
+	usleep(1000000);
+
+	while(1)
+	{
+		/* Citanje vrijednosti buttona. */
+		buttons = IORD_ALTERA_AVALON_PIO_DATA(BUTTON_0_BASE);
+
+		/*
+		 * 0xE - pritisnut KEY_0
+		 * 0xD - pritisnut KEY_1
+		 * 0xB - pritisnut KEY_2
+		 */
+		if(buttons == 0xE)
+		{
+			mode = 1;
+		}
+		else if(buttons == 0xD)
+		{
+			mode = 2;
+		}
+		else if(buttons == 0xB)
+		{
+			mode = 3;
+		}
+
+		clear_display();
+		display_mode(mode);
+
+		/* Slanje moda rada na HPS. */
+		altera_avalon_mailbox_send(mailbox_0, mode, 0, POLL);
+	}
 }
