e2d5f460f8929ae47cbefda6b1060eac4d9a9487
alexBeslic <beslic.alex@gmail.com>
2023-06-28 18:26:59 +0200

    12MHz na audio

--- de1-soc-audio/hw/quartus/soc_system/synthesis/submodules/soc_system_mm_interconnect_0_rsp_demux.sv
+++ de1-soc-audio/hw/quartus/soc_system/synthesis/submodules/soc_system_mm_interconnect_0_rsp_demux.sv
@@ -28,9 +28,9 @@
 // ------------------------------------------
 // Generation parameters:
 //   output_name:         soc_system_mm_interconnect_0_rsp_demux
-//   ST_DATA_W:           106
-//   ST_CHANNEL_W:        13
-//   NUM_OUTPUTS:         1
+//   ST_DATA_W:           129
+//   ST_CHANNEL_W:        22
+//   NUM_OUTPUTS:         2
 //   VALID_WIDTH:         1
 // ------------------------------------------
 
@@ -46,8 +46,8 @@ module soc_system_mm_interconnect_0_rsp_demux
     // Sink
     // -------------------
     input  [1-1      : 0]   sink_valid,
-    input  [106-1    : 0]   sink_data, // ST_DATA_W=106
-    input  [13-1 : 0]   sink_channel, // ST_CHANNEL_W=13
+    input  [129-1    : 0]   sink_data, // ST_DATA_W=129
+    input  [22-1 : 0]   sink_channel, // ST_CHANNEL_W=22
     input                         sink_startofpacket,
     input                         sink_endofpacket,
     output                        sink_ready,
@@ -56,11 +56,18 @@ module soc_system_mm_interconnect_0_rsp_demux
     // Sources 
     // -------------------
     output reg                      src0_valid,
-    output reg [106-1    : 0] src0_data, // ST_DATA_W=106
-    output reg [13-1 : 0] src0_channel, // ST_CHANNEL_W=13
+    output reg [129-1    : 0] src0_data, // ST_DATA_W=129
+    output reg [22-1 : 0] src0_channel, // ST_CHANNEL_W=22
     output reg                      src0_startofpacket,
     output reg                      src0_endofpacket,
     input                           src0_ready,
+
+    output reg                      src1_valid,
+    output reg [129-1    : 0] src1_data, // ST_DATA_W=129
+    output reg [22-1 : 0] src1_channel, // ST_CHANNEL_W=22
+    output reg                      src1_startofpacket,
+    output reg                      src1_endofpacket,
+    input                           src1_ready,
 
 
     // -------------------
@@ -73,7 +80,7 @@ module soc_system_mm_interconnect_0_rsp_demux
 
 );
 
-    localparam NUM_OUTPUTS = 1;
+    localparam NUM_OUTPUTS = 2;
     wire [NUM_OUTPUTS - 1 : 0] ready_vector;
 
     // -------------------
@@ -87,14 +94,22 @@ module soc_system_mm_interconnect_0_rsp_demux
 
         src0_valid         = sink_channel[0] && sink_valid;
 
+        src1_data          = sink_data;
+        src1_startofpacket = sink_startofpacket;
+        src1_endofpacket   = sink_endofpacket;
+        src1_channel       = sink_channel >> NUM_OUTPUTS;
+
+        src1_valid         = sink_channel[1] && sink_valid;
+
     end
 
     // -------------------
     // Backpressure
     // -------------------
     assign ready_vector[0] = src0_ready;
+    assign ready_vector[1] = src1_ready;
 
-    assign sink_ready = |(sink_channel & {{12{1'b0}},{ready_vector[NUM_OUTPUTS - 1 : 0]}});
+    assign sink_ready = |(sink_channel & {{20{1'b0}},{ready_vector[NUM_OUTPUTS - 1 : 0]}});
 
 endmodule
 
--- de1-soc-audio/hw/quartus/soc_system/synthesis/submodules/soc_system_pll_0.v
+++ de1-soc-audio/hw/quartus/soc_system/synthesis/submodules/soc_system_pll_0.v
@@ -19,6 +19,9 @@ module  soc_system_pll_0(
 	// interface 'outclk3'
 	output wire outclk_3,
 
+	// interface 'outclk4'
+	output wire outclk_4,
+
 	// interface 'locked'
 	output wire locked
 );
@@ -27,7 +30,7 @@ module  soc_system_pll_0(
 		.fractional_vco_multiplier("false"),
 		.reference_clock_frequency("50.0 MHz"),
 		.operation_mode("normal"),
-		.number_of_clocks(4),
+		.number_of_clocks(5),
 		.output_clock_frequency0("50.000000 MHz"),
 		.phase_shift0("0 ps"),
 		.duty_cycle0(50),
@@ -40,7 +43,7 @@ module  soc_system_pll_0(
 		.output_clock_frequency3("12.000000 MHz"),
 		.phase_shift3("0 ps"),
 		.duty_cycle3(50),
-		.output_clock_frequency4("0 MHz"),
+		.output_clock_frequency4("12.000000 MHz"),
 		.phase_shift4("0 ps"),
 		.duty_cycle4(50),
 		.output_clock_frequency5("0 MHz"),
@@ -86,7 +89,7 @@ module  soc_system_pll_0(
 		.pll_subtype("General")
 	) altera_pll_i (
 		.rst	(rst),
-		.outclk	({outclk_3, outclk_2, outclk_1, outclk_0}),
+		.outclk	({outclk_4, outclk_3, outclk_2, outclk_1, outclk_0}),
 		.locked	(locked),
 		.fboutclk	( ),
 		.fbclk	(1'b0),
