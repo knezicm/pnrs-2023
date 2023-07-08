06a161d497faa9fcd2f387dd9c9c990fe78aa0d0
alexBeslic <beslic.alex@gmail.com>
2023-07-08 10:14:38 +0200

    Dorada funkcija read i save

--- de1-soc-audio/sw/hps/app/de1_soc_audio_linux/audio.c
+++ de1-soc-audio/sw/hps/app/de1_soc_audio_linux/audio.c
@@ -8,6 +8,8 @@
 
 #include "audio.h"
 
+//#define BLOCK_RW
+
 
 uint32_t block_saved = 0;
 /**
@@ -23,9 +25,18 @@ void hps_save_sampels()
 {
 	uint32_t *sampels_buff1 = (uint32_t*) l_buffer;
 	uint32_t *sampels_buff2 = (uint32_t*) r_buffer;
-	uint32_t left_chn, right_chn;
+	uint32_t left_chn;
+//	uint32_t right_chn;
 	uint32_t message[2];
-	FILE *file = fopen(FILE_NAME, "w");
+	FILE *file;
+
+	remove(FILE_NAME);
+
+#ifdef BLOCK_RW
+	file = fopen("saved_samples.dat", "w");
+#else
+	file = fopen(FILE_NAME, "w");
+#endif
 
 	block_saved = 0;
 
@@ -54,28 +65,40 @@ void hps_save_sampels()
 		}
 		else if (message[1] == READ_BUFF_1)
 		{
-			for (int i = 0; i < BUFF_SIZE; i+=2)
+#ifdef BLOCK_RW
+
+			fwrite(sampels_buff1, sizeof(sampels_buff1[0]), BUFF_SIZE, file);
+
+#else
+			for (int i = 0; i < BUFF_SIZE; i++)
 			{
 				/* Getting left and right channels and writing them to a file */
 				left_chn = sampels_buff1[i];
-				right_chn = sampels_buff1[i + 1];
+//				right_chn = sampels_buff1[i + 1];
 
-				fprintf(file, "%u %u\n", left_chn, right_chn);
+				fprintf(file, "%u\n", left_chn);
 			}
-
+#endif
 			block_saved++;
 		}
 		else if (message[1] == READ_BUFF_2)
 		{
-			for (int i = 0; i < BUFF_SIZE; i+=2)
+#ifdef BLOCK_RW
+
+			fwrite(sampels_buff2, sizeof(sampels_buff2[0]), BUFF_SIZE, file);
+
+#else
+
+			for (int i = 0; i < BUFF_SIZE; i++)
 			{
 				/* Getting left and right channels and writing them to a file */
 				left_chn = sampels_buff2[i];
-				right_chn = sampels_buff2[i + 1];
+//				right_chn = sampels_buff2[i + 1];
 
-				fprintf(file, "%u %u\n", left_chn, right_chn);
+				fprintf(file, "%u\n", left_chn);
 			}
 
+#endif
 			block_saved++;
 		}
 	}
@@ -86,10 +109,18 @@ void hps_read_sampels()
 {
 	uint32_t *sampels_buff1 = (uint32_t*) l_buffer;
 	uint32_t *sampels_buff2 = (uint32_t*) r_buffer;
-	uint32_t left_chn, right_chn;
+	uint32_t left_chn;
+//	uint32_t right_chn;
 	uint32_t message[2];
 	uint32_t blocks_read = 0;
-	FILE *file = fopen(FILE_NAME, "r");
+	FILE *file;
+
+#ifdef BLOCK_RW
+	file = fopen("saved_samples.dat", "w");
+#else
+	file = fopen(FILE_NAME, "w");
+#endif
+
 
 	/* No recording was found*/
 	if (file == NULL)
@@ -115,21 +146,29 @@ void hps_read_sampels()
 		}
 		else if (message[1] == WRITE_BUFF_1)
 		{
-			for (int i = 0; i < BUFF_SIZE / 2; i+=2)
+#ifdef BLOCK_RW
+
+			fread(sampels_buff1, sizeof(sampels_buff1[0]), BUFF_SIZE, file);
+
+#else
+
+			for (int i = 0; i < BUFF_SIZE; i++)
 			{
 				/* Reading left and right channels from the file and putting them into buff 1*/
-				fscanf(file, "%u %u\n", &left_chn, &right_chn);
+				fscanf(file, "%u\n", &left_chn);
 
-				sampels_buff1[2 * i] = left_chn;
-				sampels_buff1[2 * i + 1] = right_chn;
+				sampels_buff1[i] = left_chn;
+//				sampels_buff1[2 * i + 1] = right_chn;
 
 				/* If we reach EOF, we start from the beginning*/
-				if(feof(file) || left_chn == 0 || right_chn == 0)
+				if(feof(file) || left_chn == 0)
 				{
 					rewind(file);
 				}
 
 			}
+
+#endif
 
 			blocks_read++;
 
@@ -141,21 +180,31 @@ void hps_read_sampels()
 		}
 		else if (message[1] == WRITE_BUFF_2)
 		{
-			for (int i = 0; i < BUFF_SIZE / 2 ; i+=2)
+
+#ifdef BLOCK_RW
+
+			fread(sampels_buff2, sizeof(sampels_buff2[0]), BUFF_SIZE, file);
+
+#else
+
+			for (int i = 0; i < BUFF_SIZE ; i++)
 			{
 				/* Reading left and right channels from the file and putting them into buff 2*/
-				fscanf(file, "%u %u\n", &left_chn, &right_chn);
+				fscanf(file, "%u\n", &left_chn);
 
-				sampels_buff2[2 * i] = left_chn;
-				sampels_buff2[2 * i + 1] = right_chn;
+				sampels_buff2[i] = left_chn;
+//				sampels_buff2[2 * i + 1] = right_chn;
 
 				/* If we reach EOF, we start from the beginning*/
-				if(feof(file) || left_chn == 0 || right_chn == 0)
+				if(feof(file) || left_chn == 0)
 				{
 					rewind(file);
 				}
 
 			}
+
+#endif
+
 			blocks_read++;
 
 			// Informing NIOS(2) that the buffer is filled
@@ -166,7 +215,7 @@ void hps_read_sampels()
 		}
 
 		/* If read more the we saved we start from the beginning*/
-		if (blocks_read == block_saved || feof(file))
+		if (feof(file))
 		{
 			blocks_read = 0;
 			rewind(file);
