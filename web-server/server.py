a3c17ab4f1efabbf9490c92bf00d7cd3a6f0d7b1
alexBeslic <beslic.alex@gmail.com>
2023-07-11 19:16:11 +0200

    Web server remake

--- web-server/server.py
+++ web-server/server.py
@@ -1,6 +1,5 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import urllib.parse
-import threading
 import subprocess
 
 VALID_FREQ = ['8', '32', '48', '96']
@@ -8,10 +7,33 @@ DEFAULT_FREQ = '8'
 VALID_WORD_LENGTH = ['16', '20', '24', '32']
 DEFAULT_WORD_LENGTH = '24'
 
-subprocess.call(['sh','/root/pass_through.sh'])
+I2C_VOLUMEN_L = "i2cset -y 0 0x1a 0x04 "
+I2C_VOLUMEN_R = "i2cset -y 0 0x1a 0x06 "
+VOL_CON = ["0x30","0x42","0x54","0x66","0x79","0x7a","0x7b","0x7c","0x7d","0x7f"]
 
-def c_function():
-	subprocess.run(["/root/de1_soc_audio_linux"])
+SOURCE_IN = ["i2cset -y 0 0x1a 0x00 0x97\ni2cset -y 0 0x1a 0x02 0x97\ni2cset -y 0 0x1a 0x08 0x14",
+             "i2cset -y 0 0x1a 0x00 0x17\ni2cset -y 0 0x1a 0x02 0x17\ni2cset -y 0 0x1a 0x08 0x12"] 
+
+# Send the HTML form to the client
+response_message = '''
+           <!DOCTYPE html>
+                <html>
+                <head>
+                </head>
+                <body>
+                    <h1>Audio configuration</h1>
+                    <form method="POST">
+                        <label for="l_volume">Left channel volume</label><br>
+                        <input type="range" id="l_volume" name="l_volume" min="0" max="9"><br>
+                        <label for="r_volume">Right channel volume</label><br>
+                        <input type="range" id="r_volume" name="r_volume" min="0" max="9"><br><br>
+                        <input type="submit" value="Submit"><br><br>
+                        <a href="https://ibb.co/XkkFF6j"><img src="https://i.ibb.co/rff55qF/IMG-0075.jpg" alt="IMG-0075" border="0"></a>
+                    </form>
+
+                </body>
+                </html>
+            '''
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -19,35 +41,6 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
 
-        # Send the HTML form to the client
-        response_message = '''
-        <html>
-        <head>
-        <title>Audio configuration</title>
-        </head>
-        <body>
-        <h1>Audio configuration</h1>
-        <form method="POST">
-        <label for="freq">Enter the sample rate in kHz (8kHz, 32kHz, 48kHz, 96kHz) :</label><br>
-        <input type="text" id="freq" name="freq"><br><br>
-        <label for="word_length">Enter the word Length: 16/20/24/32 bit :</label><br>
-        <input type="text" id="word_length" name="word_length"><br><br>
-        <label for="l_volume">Left channel volume</label><br>
-        <input type="range" id="l_volume" name="l_volume" min="0" max="9"><br>
-        <label for="r_volume">Right channel volume</label><br>
-        <input type="range" id="r_volume" name="r_volume" min="0" max="9"><br><br>
-        <fieldset style="width:230px">
-            <legend>Please select your input source:</legend>
-            <input type="radio" id="sourceMic" name="sorce" value="sourceMic" checked/>
-            <label for="contactChoice2">Microphone Input</label><br>
-            <input type="radio" id="sourceLine" name="sorce" value="sourceLine" />
-            <label for="contactChoice3">Line Input</label>
-        </fieldset><br>
-        <input type="submit" value="Submit">
-        </form>
-        </body>
-        </html>
-        '''
 
         self.wfile.write(response_message.encode('utf-8'))
 
@@ -64,38 +57,21 @@ class RequestHandler(BaseHTTPRequestHandler):
 
         # Parse the form data
         form_data = urllib.parse.parse_qs(post_data)
-        freq = form_data.get('freq', [''])[0]
-        word_length = form_data.get('word_length', [''])[0]
         l_vol = form_data.get('l_volume', [''])[0]
         r_vol = form_data.get('r_volume', [''])[0]
-        source = form_data.get('sorce', [''])[0]
         
 
-        # Check for validity
-        if (freq not in VALID_FREQ):
-            freq = DEFAULT_FREQ
+        left_vol = I2C_VOLUMEN_L + VOL_CON[int(l_vol)]
+        right_vol = I2C_VOLUMEN_R + VOL_CON[int(r_vol)]
 
-        # Check for validity
-        if (word_length not in VALID_WORD_LENGTH):
-            word_length = DEFAULT_WORD_LENGTH
-
-        # 
-        if(source == 'sourceMic'):
-            input_source = '1'
-        else:
-            input_source = '2'
-
-
-        # Write the data to a file
-        with open('audio_config.txt', 'w') as file:
-            file.write(freq + '\n')
-            file.write(word_length + '\n')
-            file.write(l_vol + '\n')
-            file.write(r_vol + '\n')
-            file.write(input_source + '\n')
+        with open('/root/audio_config.sh', 'w') as file:
+            file.write(left_vol + '\n')
+            file.write(right_vol + '\n')
+            file.close()
+            
+        subprocess.call(['sh','/root/audio_config.sh'])
 
         # Send a response message
-        response_message = 'Configuration was saved'
         self.wfile.write(response_message.encode('utf-8'))
 
         #For one time use
@@ -111,9 +87,4 @@ def run_server():
     print(f'Starting server on port {port}...')
     httpd.serve_forever()
 
-thread2 = threading.Thread(target=c_function)
-thread2.start()
-
-thread = threading.Thread(target=run_server)
-thread.start()
-#run_server(8000)
+run_server()
